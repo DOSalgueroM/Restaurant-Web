11db59f1ff81b2bc8b8d07dea3937f54ee950138
DOSalgueroM <daviniasalguero@gmail.com>
2024-09-02 17:57:53 -0400

    refactor Find more button

--- src/componentes/AboutUs.jsx
+++ src/componentes/AboutUs.jsx
@@ -2,6 +2,9 @@ import '../assets/imagerestaurant.png';
 import '../styles/AboutUs.css';
 import restaurant from '../assets/restaurant.png'; 
 
+const redirecServices = () => {
+    window.location.href = '/services';
+}
 export function AboutUs() {
     return (
         <section className="about-us">
@@ -18,7 +21,7 @@ export function AboutUs() {
                 making us one of the two hotels in Bolivia with this prestigious certification. Our commitment
                 to quality and excellence ensures that every guest enjoys their stay to the fullest.
                 </p>
-                <button className="find-more-button">Find More</button>
+                <button className="find-more-button" onClick={redirecServices} >Find More</button>
             </div>
         </section>
     );
